23f5336a28b1765b0765f85a2a167cf4457c0960
chaimae alaoui <chaimaehachimi01@gmail.com>
2023-03-17 14:38:09 -0700

    the committed files

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,8 +1,8 @@
 #include<stdio.h>
 
 /**
- *main - lowercase alpha in reverse
- *Return :Always 0 (Success)
+ *main - print lowercase alpha in reverse
+ *Return: Always 0 (Success)
  */
 
 int main(void)
