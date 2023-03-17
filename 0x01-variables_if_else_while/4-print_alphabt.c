d7de92794b3e72a7c349837e211b4a1d290eaa9b
chaimae alaoui <chaimaehachimi01@gmail.com>
2023-03-17 13:47:23 -0700

    the committed files

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,13 +1,17 @@
 #include<stdio.h>
+
 /**
- *main : print all the letters except q and e using only outchar twice
+ *main - print all the letters except q and e using only outchar twice
+ *Return: Always 0 (Success)
  */
+
 int main(void)
 {
 	char letter = 'a';
+
 	while (letter <= 'z')
 	{
-		if ((letter != 'e' ) && (letter != 'q'))
+		if ((letter != 'e') && (letter != 'q'))
 		{
 			putchar(letter);
 		}
