da017488f57c114ce4d179110a9cb3bc65bdb16b
chaimae alaoui <chaimaehachimi01@gmail.com>
2023-03-17 13:18:13 -0700

    the committed files

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,4 @@
 #include<stdio.h>
-#include <stdlib.h>
-#include <time.h>
 /**
  * main - Entry point
  *
@@ -8,11 +6,15 @@
  */
 int main(void)
 {
-	char low;
+	char letter = 'a';
 
-	for (low = 'a'; low <= 'z'; low++)
-		putchar(low);
-		printf("\n");
+	while (letter <= 'z')
+	{
+		putchar(letter);
+		letter++;
+	}
+
+	putchar('\n');
 
 	return (0);
 }
