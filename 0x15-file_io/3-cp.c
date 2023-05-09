275d12f75d618e294ad5de14a5be7eefa1fb18e0
chaimae-al <chaimaehachimi01@gmail.com>
2023-05-09 02:07:20 +0100

    ok

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -83,7 +83,7 @@ int main(int argc, char *argv[])
 		w = write(dst, buffer, r);
 		if (dst == -1 || w == -1)
 		{
-			dprintf(STDERR_FILENO,"Error: Can't write to %s\n", argv[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
